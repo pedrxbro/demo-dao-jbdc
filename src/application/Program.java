ca5edbcc9204b68489151fe1426ba3845510225a
pedrxbro <pedrohdpaula@edu.univali.br>
2024-12-10 22:19:27 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,12 +9,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-
-        Department department = new Department(1, "Books");
-        Seller seller = new Seller(21, "Pedro", "pedro@gmail.com", new Date(), 3000, department);
-
         //Instanciando o SellerDao com factory
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJBDC;
+
+import java.sql.Connection;
 
 public class DaoFactory {
     public static SellerDaoJBDC createSellerDao(){
-        return new SellerDaoJBDC();
+        return new SellerDaoJBDC(DB.getConnection());
     }
 }
